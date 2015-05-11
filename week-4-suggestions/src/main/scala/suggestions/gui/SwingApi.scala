5059fc133e61995a0655ebdd87859ecb5693ee46
Paul-Guillaume Déjardin <paul-guillaume.dejardin@ei-technologies.com>
2015-05-11 09:42:37 +0200

    week4 end

--- week-4-suggestions/src/main/scala/suggestions/gui/SwingApi.scala
+++ week-4-suggestions/src/main/scala/suggestions/gui/SwingApi.scala
@@ -1,24 +1,20 @@
 package suggestions
 package gui
 
+import rx.lang.scala.{Observable, Subscription}
+
 import scala.language.reflectiveCalls
-import scala.collection.mutable.ListBuffer
-import scala.collection.JavaConverters._
-import scala.concurrent._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{ Try, Success, Failure }
 import scala.swing.Reactions.Reaction
 import scala.swing.event.Event
-import rx.lang.scala.{Observer, Subscription, Observable}
 
 /** Basic facilities for dealing with Swing-like components.
-*
-* Instead of committing to a particular widget implementation
-* functionality has been factored out here to deal only with
-* abstract types like `ValueChanged` or `TextField`.
-* Extractors for abstract events like `ValueChanged` have also
-* been factored out into corresponding abstract `val`s.
-*/
+  *
+  * Instead of committing to a particular widget implementation
+  * functionality has been factored out here to deal only with
+  * abstract types like `ValueChanged` or `TextField`.
+  * Extractors for abstract events like `ValueChanged` have also
+  * been factored out into corresponding abstract `val`s.
+  */
 trait SwingApi {
 
   type ValueChanged <: Event
@@ -51,20 +47,14 @@ trait SwingApi {
       * @param field the text field
       * @return an observable with a stream of text field updates
       */
-    def textValues: Observable[String] = { field: String =>
-      Observable[String]((observer: Observer[String]) => {
-        field subscribe {
-          case ValueChanged(tf) => observer.onNext(tf.text)
-          case _ => ()
-        }
-
-        Subscription {
-          field unsubscribe {
-            case ValueChanged(tf) => observer.onCompleted
-            case _ => ()
-          }
-        }
-      })
+    def textValues: Observable[String] = Observable { observer =>
+      val rx: Reaction = {
+        case ValueChanged(x) => observer.onNext(x.text)
+      }
+      field.subscribe(rx)
+      Subscription {
+        field.unsubscribe(rx)
+      }
     }
 
   }
@@ -72,11 +62,19 @@ trait SwingApi {
   implicit class ButtonOps(button: Button) {
 
     /** Returns a stream of button clicks.
-     *
-     * @param field the button
-     * @return an observable with a stream of buttons that have been clicked
-     */
-    def clicks: Observable[Button] = ???
+      *
+      * @param field the button
+      * @return an observable with a stream of buttons that have been clicked
+      */
+    def clicks: Observable[Button] = Observable { observer =>
+      val rx: Reaction = {
+        case ButtonClicked(x) => observer.onNext(x)
+      }
+      button.subscribe(rx)
+      Subscription {
+        button.unsubscribe(rx)
+      }
+    }
   }
 
 }
--- week-4-suggestions/src/main/scala/suggestions/gui/WikipediaSuggest.scala
+++ week-4-suggestions/src/main/scala/suggestions/gui/WikipediaSuggest.scala
@@ -1,21 +1,18 @@
 package suggestions
 package gui
 
-import scala.collection.mutable.ListBuffer
-import scala.collection.JavaConverters._
-import scala.concurrent._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.swing._
-import scala.util.{ Try, Success, Failure }
-import scala.swing.event._
-import swing.Swing._
 import javax.swing.UIManager
-import Orientation._
-import rx.subscriptions.CompositeSubscription
-import rx.lang.scala.Observable
-import rx.lang.scala.Subscription
-import observablex._
-import search._
+
+import rx.lang.scala.{Observable, Subscription}
+import suggestions.observablex._
+import suggestions.search._
+
+import scala.collection.mutable.ListBuffer
+import scala.swing.Orientation._
+import scala.swing.Swing._
+import scala.swing._
+import scala.swing.event._
+import scala.util.{Success, Failure, Try}
 
 object WikipediaSuggest extends SimpleSwingApplication with ConcreteSwingApi with ConcreteWikipediaApi {
 
@@ -41,7 +38,9 @@ object WikipediaSuggest extends SimpleSwingApplication with ConcreteSwingApi wit
     val suggestionList = new ListView(ListBuffer[String]())
     val status = new Label(" ")
     val editorpane = new EditorPane {
+
       import javax.swing.border._
+
       border = new EtchedBorder(EtchedBorder.LOWERED)
       editable = false
       peer.setContentType("text/html")
@@ -74,32 +73,43 @@ object WikipediaSuggest extends SimpleSwingApplication with ConcreteSwingApi wit
     /**
      * Observables
      * You may find the following methods useful when manipulating GUI elements:
-     *  `myListView.listData = aList` : sets the content of `myListView` to `aList`
-     *  `myTextField.text = "react"` : sets the content of `myTextField` to "react"
-     *  `myListView.selection.items` returns a list of selected items from `myListView`
-     *  `myEditorPane.text = "act"` : sets the content of `myEditorPane` to "act"
+     * `myListView.listData = aList` : sets the content of `myListView` to `aList`
+     * `myTextField.text = "react"` : sets the content of `myTextField` to "react"
+     * `myListView.selection.items` returns a list of selected items from `myListView`
+     * `myEditorPane.text = "act"` : sets the content of `myEditorPane` to "act"
      */
 
     // TO IMPLEMENT
-    val searchTerms: Observable[String] = ???
+    val searchTerms: Observable[String] = searchTermField.textValues.sanitized
 
     // TO IMPLEMENT
-    val suggestions: Observable[Try[List[String]]] = ???
+    val suggestions: Observable[Try[List[String]]] = searchTerms.flatMap(wikiSuggestResponseStream).recovered
 
     // TO IMPLEMENT
-    val suggestionSubscription: Subscription =  suggestions.observeOn(eventScheduler) subscribe {
-      x => ???
+    val suggestionSubscription: Subscription = suggestions.observeOn(eventScheduler) subscribe {
+      x => x match {
+        case Success(s) => suggestionList.listData = s
+        case Failure(e) => status.text = e.getLocalizedMessage
+      }
     }
 
     // TO IMPLEMENT
-    val selections: Observable[String] = ???
+    val selections: Observable[String] = button.clicks.map(a => suggestionList.selection.items.head)
 
     // TO IMPLEMENT
-    val pages: Observable[Try[String]] = ???
+    val pages: Observable[Try[String]] = selections.flatMap(wikiPageResponseStream).recovered
+
+    val pageSubFunc: PartialFunction[Try[String], Unit] = {
+      case Success(v) => editorpane.text = v
+      case Failure(e) => status.text = e.getMessage
+    }
 
     // TO IMPLEMENT
     val pageSubscription: Subscription = pages.observeOn(eventScheduler) subscribe {
-      x => ???
+      x => x match {
+        case Success(s) => editorpane.text = s
+        case Failure(e) => status.text = e.getLocalizedMessage
+      }
     }
 
   }
@@ -109,25 +119,30 @@ object WikipediaSuggest extends SimpleSwingApplication with ConcreteSwingApi wit
 
 trait ConcreteWikipediaApi extends WikipediaApi {
   def wikipediaSuggestion(term: String) = Search.wikipediaSuggestion(term)
+
   def wikipediaPage(term: String) = Search.wikipediaPage(term)
 }
 
 
 trait ConcreteSwingApi extends SwingApi {
   type ValueChanged = scala.swing.event.ValueChanged
+
   object ValueChanged {
     def unapply(x: Event) = x match {
       case vc: ValueChanged => Some(vc.source.asInstanceOf[TextField])
       case _ => None
     }
   }
+
   type ButtonClicked = scala.swing.event.ButtonClicked
+
   object ButtonClicked {
     def unapply(x: Event) = x match {
       case bc: ButtonClicked => Some(bc.source.asInstanceOf[Button])
       case _ => None
     }
   }
+
   type TextField = scala.swing.TextField
   type Button = scala.swing.Button
 }
